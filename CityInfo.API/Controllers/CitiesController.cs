4a57b10c8d54d58fa967c7adb7eaec63333f4ed8
Angelito Sardez, Jr <jojosardez@gmail.com>
2018-01-25 22:27:35 +1100

    Added attribute based routing to the action

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -1,13 +1,12 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 
 namespace CityInfo.API.Controllers
 {
+    [Route("api/cities")]
     public class CitiesController : Controller
     {
+        [HttpGet()]
         public JsonResult GetCities()
         {
             return new JsonResult(new List<object>()
