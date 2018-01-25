2d9d2e375f38fb2a9679dcfe47787edc6d9bf17a
Angelito Sardez, Jr <jojosardez@gmail.com>
2018-01-25 23:34:51 +1100

    Added output formatter to allow accept headers that expects XML

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CityInfo.API
@@ -11,7 +12,21 @@ namespace CityInfo.API
         public void ConfigureServices(IServiceCollection services)
         {
             // Add MVC middleware
-            services.AddMvc();
+            services.AddMvc()
+
+                // Formmater option to support Accept headers in request that asks for XML
+                .AddMvcOptions(o => o.OutputFormatters.Add(
+                    new XmlDataContractSerializerOutputFormatter()));
+
+                // Serialization option to return JSON property names in their original casing.
+                //.AddJsonOptions(o =>
+                //{
+                //    if (o.SerializerSettings.ContractResolver != null)
+                //    {
+                //        var casterResolver = o.SerializerSettings.ContractResolver as DefaultContractResolver;
+                //        casterResolver.NamingStrategy = null;
+                //    }
+                //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
