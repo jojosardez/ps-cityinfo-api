5dae67ae118874c3ec3ebcf0b5c5fff8add0ddda
Angelito Sardez, Jr <jojosardez@gmail.com>
2018-01-25 22:04:22 +1100

    Added MVC middleware to the application and used it in the request pipeline

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,6 +11,8 @@ namespace CityInfo.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add MVC middleware
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,6 +22,13 @@ namespace CityInfo.API
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler();
+            }
+
+            // Add MVC middleware to the request pipeline
+            app.UseMvc();
 
             app.Run(async (context) =>
             {
