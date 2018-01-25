39b2f83a0fc1d11d213964b8e47cfc4bc5fc7b7e
Angelito Sardez, Jr <jojosardez@gmail.com>
2018-01-25 23:18:58 +1100

    Implemented retrieval of child resource - points of interest

--- CityInfo.API/CitiesDataStore.cs
+++ CityInfo.API/CitiesDataStore.cs
@@ -15,19 +15,64 @@ namespace CityInfo.API
                 {
                     Id = 1,
                     Name = "Melbourne",
-                    Description = "One of the most liveable cities in the world."
+                    Description = "One of the most liveable cities in the world.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto
+                        {
+                            Id = 1,
+                            Name = "Flinder's Street Railway Station",
+                            Description = "Melbourne's iconic train station."
+                        },
+                        new PointOfInterestDto
+                        {
+                            Id = 2,
+                            Name = "Shrine of Remembrance",
+                            Description = "A popular tourist spot in the city."
+                        }
+                    }
                 },
                 new CityDto
                 {
                     Id = 2,
                     Name = "Singapore",
-                    Description = "One of the cities with the highest standard of living."
+                    Description = "One of the cities with the highest standard of living.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto
+                        {
+                            Id = 3,
+                            Name = "Merlion Park",
+                            Description = "This is where the iconic Merlion statue is located."
+                        },
+                        new PointOfInterestDto
+                        {
+                            Id = 4,
+                            Name = "Gardens by the Bay",
+                            Description = "A garden in the city enclosed in huge glass domes."
+                        }
+                    }
                 },
                 new CityDto
                 {
                     Id = 3,
                     Name = "Manila",
-                    Description = "Capital city of the Philippines."
+                    Description = "Capital city of the Philippines.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto
+                        {
+                            Id = 5,
+                            Name = "Intramuros",
+                            Description = "Known as the walled city, it was the former base of Spain's governor general to the Philippines."
+                        },
+                        new PointOfInterestDto
+                        {
+                            Id = 6,
+                            Name = "Manila Bay",
+                            Description = "Known for its perfect sunset."
+                        }
+                    }
                 }
             };
         }
---
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/cities")]
+    public class PointsOfInterestController : Controller
+    {
+        [HttpGet("{cityId}/pointsofinterest")]
+        public IActionResult GetPointsOfInterest(int cityId)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+            return Ok(city.PointsOfInterest);
+        }
+
+        [HttpGet("{cityId}/pointsofinterest/{id}")]
+        public IActionResult GetPointsOfInterest(int cityId, int id)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterest = city.PointsOfInterest.FirstOrDefault(p => p.Id == id);
+            if (pointOfInterest == null)
+            {
+                return NotFound();
+            }
+            return Ok(pointOfInterest);
+        }
+    }
+}
