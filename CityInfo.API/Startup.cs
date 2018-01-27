f10a3cc16fc0ba6f50ecb518b64af642c089078e
Angelito Sardez, Jr <jojosardez@gmail.com>
2018-01-27 23:24:21 +1100

    Add dbcontext

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -1,7 +1,9 @@
-﻿using CityInfo.API.Services;
+﻿using CityInfo.API.Entities;
+using CityInfo.API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -66,6 +68,9 @@ namespace CityInfo.API
 #else
             services.AddTransient<IMailService, CloudMailService>();
 #endif
+
+            var connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Database=CityInfoDb;Trusted_Connection=True;";
+            services.AddDbContext<CityInfoContext>(o => o.UseSqlServer(connectionString));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
